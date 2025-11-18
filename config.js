23673cfd74a4f9b9b7b9124383af4914556884cc
tycoon-254 <deputysherif911@gmail.com>
2025-11-18 12:23:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,26 +16,26 @@ module.exports = {
         ? process.env.PREFIX.split(',').map(p => p.trim())
         : [''],
 
-    NUMBER: process.env.YOUR_NUMBER || '254742063632',
+    NUMBER: process.env.YOUR_NUMBER || '254789995355',
     MODE: (process.env.MODE || 'private').toLowerCase().trim(),
     WARN_LIMIT: process.env.WARNINGS || '3',
-    ON: process.env.YOUR_NAME || 'FLASH-MD',
-    ANTICALL: process.env.ANTICALL || 'on',
+    ON: process.env.YOUR_NAME || 'Khynn',
+    ANTICALL: process.env.ANTICALL || 'off',
     ADM: process.env.ANTIDELETE || 'on',
     AR: process.env.AUTO_REACTION || 'off',
 
     AUTO_VIEW_STATUS: process.env.AUTO_READ_STATUS === 'on',
     AUTO_LIKE: process.env.AUTO_LIKE === 'on',
-    AUTO_READ_MESSAGES: process.env.AUTO_READ_DM === 'on',
-    HEROKU_API_KEY: process.env.HEROKU_API_KEY,
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_DM === 'off',
+    HEROKU_API_KEY: 
     HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
     sessionBase64: process.env.SESSION || '',
     timezone: 'Africa/Nairobi',
 
     USER_LID: process.env.YOUR_LID, 
 
-    PRESENCE_DM: mapPresence(process.env.PRESENCE_DM || 'typing'),
-    PRESENCE_GROUP: mapPresence(process.env.PRESENCE_GROUP || 'recording'),
+    PRESENCE_DM: mapPresence(process.env.PRESENCE_DM || 'online'),
+    PRESENCE_GROUP: mapPresence(process.env.PRESENCE_GROUP || 'online'),
 
     mapPresence
 };
